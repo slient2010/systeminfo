33a8f9861dc8d6a9ba9a4f7f5c6142bcd203e461
slient2010 <slient2010@163.com>
2019-10-29 21:57:41 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ use systemstat::{System, Platform, saturating_sub_bytes};
 
 fn main() {
     let sys = System::new();
-    // 获取挂载磁盘信息
-    match sys.mounts() {
-        Ok(mounts) => {
-            println!("\nMounts:");
-            for mount in mounts.iter() {
-                println!("{} ---{}---> {} (available {} of {})",
-                         mount.fs_mounted_from, mount.fs_type, mount.fs_mounted_on, mount.avail, mount.total);
-            }
-        }
-        Err(x) => println!("\nMounts: error: {}", x)
-    }
+    // // 获取挂载文件信息
+    // match sys.mounts() {
+    //     Ok(mounts) => {
+    //         println!("\nMounts:");
+    //         for mount in mounts.iter() {
+    //             println!("{} ---{}---> {} (available {} of {})",
+    //                      mount.fs_mounted_from, mount.fs_type, mount.fs_mounted_on, mount.avail, mount.total);
+    //         }
+    //     }
+    //     Err(x) => println!("\nMounts: error: {}", x)
+    // }
 
-    //
+    // 获取挂磁盘信息
     match sys.block_device_statistics() {
         Ok(stats) => {
             for blkstats in stats.values() {
@@ -28,6 +28,7 @@ fn main() {
         Err(x) => println!("\nBlock statistics error: {}", x.to_string())
     }
 
+    // 获取网卡信息
     match sys.networks() {
         Ok(netifs) => {
             println!("\nNetworks:");
@@ -38,6 +39,7 @@ fn main() {
         Err(x) => println!("\nNetworks: error: {}", x)
     }
 
+    // 获取网卡流量信息
     match sys.networks() {
         Ok(netifs) => {
             println!("\nNetwork interface statistics:");
@@ -48,6 +50,7 @@ fn main() {
         Err(x) => println!("\nNetworks: error: {}", x)
     }
 
+    // 获取电池状态
     match sys.battery_life() {
         Ok(battery) =>
             print!("\nBattery: {}%, {}h{}m remaining",
@@ -57,31 +60,37 @@ fn main() {
         Err(x) => print!("\nBattery: error: {}", x)
     }
     
+    // 获取系统开机信息
     match sys.on_ac_power() {
         Ok(power) => println!(", AC power: {}", power),
         Err(x) => println!(", AC power: error: {}", x)
     }
 
+    //获取内存情况
     match sys.memory() {
         Ok(mem) => println!("\nMemory: {} used / {} ({} bytes) total ({:?})", saturating_sub_bytes(mem.total, mem.free), mem.total, mem.total.as_u64(), mem.platform_memory),
         Err(x) => println!("\nMemory: error: {}", x)
     }
 
+    // 获取当前系统负载情况
     match sys.load_average() {
         Ok(loadavg) => println!("\nLoad average: {} {} {}", loadavg.one, loadavg.five, loadavg.fifteen),
         Err(x) => println!("\nLoad average: error: {}", x)
     }
 
+    // 获取开机时间
     match sys.uptime() {
         Ok(uptime) => println!("\nUptime: {:?}", uptime),
         Err(x) => println!("\nUptime: error: {}", x)
     }
 
+    // 获取系统启动时间
     match sys.boot_time() {
         Ok(boot_time) => println!("\nBoot time: {}", boot_time),
         Err(x) => println!("\nBoot time: error: {}", x)
     }
 
+    // CPU 负载
     match sys.cpu_load_aggregate() {
         Ok(cpu)=> {
             println!("\nMeasuring CPU load...");
@@ -93,14 +102,14 @@ fn main() {
         Err(x) => println!("\nCPU load: error: {}", x)
     }
 
+    // 获取CPU 温度
     match sys.cpu_temp() {
         Ok(cpu_temp) => println!("\nCPU temp: {}", cpu_temp),
         Err(x) => println!("\nCPU temp: {}", x)
     }
-
+    // 获取socket状态
     match sys.socket_stats() {
         Ok(stats) => println!("\nSystem socket statistics: {:?}", stats),
         Err(x) => println!("\nSystem socket statistics: error: {}", x.to_string())
     }
-
 }
